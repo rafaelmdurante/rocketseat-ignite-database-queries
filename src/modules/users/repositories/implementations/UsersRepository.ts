db9296c6e4c762ed7b058b3b75790ca26477f346
Rafael Miranda <rafaelmdurante@gmail.com>
2021-10-24 12:00:15 +0100

    complete tests

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,27 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.createQueryBuilder('user')
+      .leftJoinAndSelect('user.games', 'game')
+      .where('user.id = :id', { id: user_id })
+      .getOne()
+
+    return user
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `SELECT * FROM users ORDER BY first_name ASC`
+    ); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      'SELECT * FROM users AS u WHERE LOWER(u.first_name) = $1 AND LOWER(u.last_name) = $2',
+      [ first_name.toLowerCase(), last_name.toLowerCase() ]
+    ); 
   }
 }
